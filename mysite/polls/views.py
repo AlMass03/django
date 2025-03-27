a7fbb69465d9e228164d4cc3e0aecc4979694464
Masalych <101625603+AlMass03@users.noreply.github.com>
2025-03-27 09:41:58 +0000

    obrashenie k klassam

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
+from .models import User, Container, Rental, SibTransLog, Notification    #подгрузил из models все классы
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def dataset_view(request):
+    users = User.objects.all()
+    containers = Container.objects.all()
+    rentals = Rental.objects.all()
+    sibtranslogs = SibTransLog.objects.all()
+    notifications = Notification.objects.all()
+
+    return render(request, 'dataset.html', {      #возвращает через рендер в файл  dataset
+        'users': users,
+        'containers': containers,
+        'rentals': rentals,
+        'sibtranslogs': sibtranslogs,
+        'notifications': notifications
+    }) 
